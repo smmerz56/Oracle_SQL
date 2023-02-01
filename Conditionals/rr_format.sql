90aa60022d952b65450ad2aef00cf6d519c96c18
Sean Merz <72666129+smmerz56@users.noreply.github.com>
2023-02-01 12:28:44 -0800

    Delete rr_format.sql

--- Conditionals/rr_format.sql
+++
@@ -1,5 +0,0 @@
-select last_name, to_char(hire_date, 'DD-MON-yyyy')
-from employees
-where to_date(hire_date, 'DD-Mon-RR') < '01-Jan-96';
---This will not work if using the YY format in the WHERE clause 
---  because it assumes the current century
