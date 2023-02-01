6d5752b4a22b2c806e6d8393e71dca5bdceefa0b
Sean Merz <72666129+smmerz56@users.noreply.github.com>
2023-02-01 10:57:42 -0800

    Delete NVL.sql

--- NVL.sql
+++
@@ -1,9 +0,0 @@
---If the first value in the NVL function is null, then it returns
---  'No comission'. If not null, then it returns the string version
---  of comission.
-select last_name, NVL(to_char(commission_pct), 'No commission') "Commission"
-from employees;
-
---If first is not null, then return 'Manager'. If null, return No manager
-select last_name, NVL2(to_char(manager_id),'Manager','No manager') "Manager"
-from Employees;
